7a70d857fe828c7213af3b1b6b836bb577804bd2
andrey lazar <andreylazarmr@gmail.com>
2023-02-22 22:42:48 +0300

    task27

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+string num = Console.ReadLine();
+
+int[] Array = new int[num.Length];
+for (int i = 0; i < num.Length; i++)
+{
+    Array[i] = int.Parse(num[i] + "");
+}
+
+void GetSumValue(int[] Number)
+{
+    int result = 0;
+    for (int i = 0; i < Number.Length; i++)
+    {
+        result = result + Number[i];
+    }
+    Console.Write($"Сумма цифр в числе {num} = {result}");
+}
+
+GetSumValue(Array);
+
+
+
+
+    
