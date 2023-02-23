5c1eeac5ccbaf71f3f45f4a42a90fd34a2657a74
andrey lazar <andreylazarmr@gmail.com>
2023-02-23 11:03:30 +0300

    task29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void FillArray(int[] collection)
+{
+    int num = collection.Length;
+    int index = 0;
+    while (index < num)
+    {
+        collection[index] = new Random().Next(1, collection.Length);
+        index++;
+    }
+}
+
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for(int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int [] Array = new int [num];
+
+FillArray(Array);
+PrintArray(Array);
